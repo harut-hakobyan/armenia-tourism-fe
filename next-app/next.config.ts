8de5004ebf0eba30b3fa18c7d8a805fe77482346
Harut Hakobyan <harut.hakobyan2013@mail.ru>
2026-09-17 16:13:57 +0400

    fix deploy error fe

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+  },
 };
 
 export default nextConfig;
